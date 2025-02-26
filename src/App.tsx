02a77ff3f73884707dd17abdcc0b7e3284dba01a
Deyner Navarro <deynernavarro@estudiantec.cr>
2025-02-26 14:41:19 -0600

    Implement gMap context

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,30 @@ import { Grid } from "@radix-ui/themes";
 import GoogleMap from "./components/GoogleMap/GoogleMap";
 import PlacesList from "./components/PlacesList/PlacesList";
 import OptionPanel from "./components/PlacesPanel/OptionsPanel";
+import { GMapProvider } from "./context/GMapContext/GMapContext";
+import { PlacesProvider } from "./context/PlacesContext/PlacesContext";
 
 function App() {
 	return (
-		<Grid
-			width={"100vw"}
-			height={"100vh"}
-			columns={{ initial: "1", sm: "1fr 1fr 2fr" }}
-			rows={{ initial: "2", sm: "1" }}
-		>
-			{/* Options panel */}
-			<OptionPanel />
+		<PlacesProvider>
+			<GMapProvider>
+				<Grid
+					width={"100vw"}
+					height={"100vh"}
+					columns={{ initial: "1", sm: "1fr 1fr 2fr" }}
+					rows={{ initial: "2", sm: "1" }}
+				>
+					{/* Options panel */}
+					<OptionPanel />
 
-			{/* My places component */}
-			<PlacesList />
+					{/* My places component */}
+					<PlacesList />
 
-			{/* Google map component */}
-			<GoogleMap />
-		</Grid>
+					{/* Google map component */}
+					<GoogleMap />
+				</Grid>
+			</GMapProvider>
+		</PlacesProvider>
 	);
 }
 
