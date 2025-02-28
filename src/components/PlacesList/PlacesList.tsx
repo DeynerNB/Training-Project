199e129932cc91887e4d869dad365f9874bbad53
Deyner Navarro <deynernavarro@estudiantec.cr>
2025-02-28 08:38:29 -0600

    Add placeholder when there is no places added

--- src/components/PlacesList/PlacesList.tsx
+++ src/components/PlacesList/PlacesList.tsx
@@ -1,9 +1,11 @@
-import { Box, Grid } from "@radix-ui/themes";
-import { useContext, useEffect } from "react";
+import { Box, Flex, Grid, Text } from "@radix-ui/themes";
+import { useContext } from "react";
 import PlaceCard from "../PlaceCard/PlaceCard";
 
 import { GMapContext } from "../../context/GMapContext/GMapContext";
 import type { IPlace } from "../../interfaces/Places.interface";
+
+import style from "./PlacesList.module.scss";
 
 function PlacesList() {
 	const { placesList, removePlaceFromMap } = useContext(GMapContext);
@@ -13,22 +15,44 @@ function PlacesList() {
 	};
 
 	return (
-		<Grid
-			p={"3"}
-			gap={"3"}
-			overflowY={"scroll"}
-			rows={"repeat(auto-fill, 300px)"}
-		>
-			{placesList.map((place: IPlace) => {
-				return (
-					<PlaceCard
-						key={place.name}
-						placeData={place}
-						handleRemovePlace={handleRemovePlace}
-					/>
-				);
-			})}
-		</Grid>
+		<>
+			{placesList.length === 0 ? (
+				<Box p={"3"}>
+					<Flex
+						p={"3"}
+						height={"100%"}
+						direction={"column"}
+						justify={"center"}
+						align={"center"}
+						className={style["container-no-places-yet"]}
+					>
+						<Text align={"center"} as={"p"}>
+							There are no places added yet.
+						</Text>
+						<Text align={"center"} as={"p"}>
+							To add one, click the place you want to add on the map.
+						</Text>
+					</Flex>
+				</Box>
+			) : (
+				<Grid
+					p={"3"}
+					gap={"3"}
+					overflowY={"scroll"}
+					rows={"repeat(auto-fill, 300px)"}
+				>
+					{placesList.map((place: IPlace) => {
+						return (
+							<PlaceCard
+								key={place.name}
+								placeData={place}
+								handleRemovePlace={handleRemovePlace}
+							/>
+						);
+					})}
+				</Grid>
+			)}
+		</>
 	);
 }
 
